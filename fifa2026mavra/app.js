ee188ed1cb0c94681186fe7177f42b9f5ff750d2
aalptekin <aalptekin@yahoo.com>
2026-06-10 23:04:10 +0400

    Add files via upload

--- fifa2026mavra/app.js
+++ fifa2026mavra/app.js
@@ -7,7 +7,7 @@ Parse.initialize(
   "burDaliPLstPolKtA7ozgIUXgvgIAbsEDDRRaall",
   "nlAyYXfDQpZY7cMixJCodO0rGpOrt9iAfjKfvYLd"
 );
-Parse.serverURL = "http://burda-aaa-01.herokuapp.com/parse";
+Parse.serverURL = "https://burda-aaa-01.herokuapp.com/parse";
 
 // ── LOCALE DETECTION ───────────────────────────────────────────────
 const userLang = (navigator.language || navigator.userLanguage || "en").toLowerCase();
